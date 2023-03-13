08ad70299cc0092dcdf592e17781dcc109b56723
Deepali Verma <41052882+VermaDeepali@users.noreply.github.com>
2023-03-13 22:14:18 +0530

    write-file

--- fs-module.js
+++ fs-module.js
@@ -16,3 +16,26 @@ fs.readFile(filePath, (err, data) => {
     console.error(err);
   });
 });
+
+
+// write file if the given path doesn't exist create the path and write file
+
+const fsp = require('fs/promises');
+const fs = require('fs');
+let filePath = './temp/example.txt';
+
+async function createFile() {
+  if (!fs.existsSync(filePath)) {
+    await fsp.mkdir('./temp', { recursive: true });
+  }
+
+  await fsp.writeFile(filePath, 'write your file content here', { encoding: 'utf-8' })
+    .then(() => {
+      console.log('File created!!');
+    })
+    .catch((error) => {
+      console.error('Error creating file:', error);
+    });
+}
+
+createFile();
